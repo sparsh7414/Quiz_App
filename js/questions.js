9e867a5af50b37882227fec0f6fdc05f11181370
Sparsh Bhardwaj <sparsh.Bhardwaj@zimozi.co>
2021-11-29 15:34:01 +0530

    First Commit

---
+++ js/questions.js
@@ -0,0 +1,71 @@
+let questions = [
+    {
+    numb: 1,
+    question: "India is a federal union comprising twenty-eight states and how many union territories?",
+    answer: "8",
+    options: [
+      "6",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is the capital of Arunachal Pradesh?",
+    answer: "Itanagar",
+    options: [
+      "Itanagar",
+      "Dispur",
+      "Imphal",
+      "Panaji"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What are the major languages spoken in Andhra Pradesh?",
+    answer: "Telugu and Urdu",
+    options: [
+      "Odia and Telugu",
+      "Telugu and Urdu",
+      "Telugu and Kannada",
+      "All of the above languages"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the state flower of Haryana?",
+    answer: "Lotus",
+    options: [
+      "Lotus",
+      "Rhododendron",
+      "Golden Shower",
+      "Not declared"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following states is not located in the North?",
+    answer: "Jharkhand",
+    options: [
+      "Jharkhand",
+      "Jammu and Kashmir",
+      "Himachal Pradesh",
+      "Haryana"
+    ]
+  },
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+];
